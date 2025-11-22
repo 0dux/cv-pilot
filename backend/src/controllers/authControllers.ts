f4e8923696d43051dff298010b9c2abd55a237e2
Daksh Yadav <dakshdav@gmail.com>
2025-11-23 00:02:36 +0530

    auth controllers almost completegit add .

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -1,8 +1,12 @@
 import type { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { User } from "../db/db.js";
-import { registerUserData, type RegisterUserData } from "../types/schema.js";
-
+import {
+  loginUserData,
+  registerUserData,
+  type LoginUserData,
+  type RegisterUserData,
+} from "../types/schema.js";
 
 //Register a new user
 export const registerUser = async (req: Request, res: Response) => {
@@ -25,6 +29,7 @@ export const registerUser = async (req: Request, res: Response) => {
       res.status(409).json({
         message: "User with this email already exists!!!",
       });
+      return;
     }
 
     //create new user
@@ -45,6 +50,54 @@ export const registerUser = async (req: Request, res: Response) => {
       message: "User registered succesfully!!!",
       token,
     });
+    return;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Login user
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const loginData: LoginUserData = req.body;
+
+    //validate input data
+    const result = loginUserData.safeParse(loginData);
+    if (!result.success) {
+      res.status(400).json({
+        message: "Invalid data passed!!!",
+      });
+      return;
+    }
+
+    const { email, password } = result.data;
+
+    if (!(await User.findOne({ email }))) {
+      res.status(409).json({
+        message: "User with this email doesn't exists!!!",
+      });
+      return;
+    }
+
+    const userFound = await User.findOne({ email, password });
+    console.log(userFound);
+    if (!userFound) {
+      res.status(400).json({
+        message: "Email or password is incorrect!!!",
+      });
+      return;
+    }
+
+    const secret = process.env["JWT_SECRET"];
+    if (!secret) throw new Error("JWT_SECRET not recieved!!!");
+
+    const token = jwt.sign({ userId: userFound._id }, secret);
+
+    res.json({
+      message: "Logged in succesfully!!!",
+      token,
+    });
+    return;
   } catch (error) {
     console.log(error);
   }
--- backend/src/types/schema.ts
+++ backend/src/types/schema.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import z, { email } from "zod";
 
 export const registerUserData = z.object({
   name: z.string().min(3),
@@ -7,3 +7,10 @@ export const registerUserData = z.object({
 });
 
 export type RegisterUserData = z.infer<typeof registerUserData>;
+
+export const loginUserData = z.object({
+  email: z.email().min(3),
+  password: z.string().min(6),
+});
+
+export type LoginUserData = z.infer<typeof loginUserData>;
