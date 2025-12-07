ff894e4e6dcd04fc2e486a787a000eec32a731f8
Daksh Yadav <dakshdav@gmail.com>
2025-12-08 01:15:26 +0530

    Got some design ideas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { Navigate, Route, Routes } from "react-router";
+import AnalysisPage from "./pages/AnalysisPage";
+import LandingPage from "./pages/LandingPage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import UploadPage from "./pages/UploadPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to={"/home"} />} />
+      <Route path="/home" element={<LandingPage />} />
+      <Route path="/login" element={<SignInPage />} />
+      <Route path="/register" element={<SignUpPage />} />
+      <Route path="/upload" element={<UploadPage />} />
+      <Route path="/analysis" element={<AnalysisPage />} />
+    </Routes>
+  );
 };
 
 export default App;
