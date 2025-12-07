c3d4ce11ec3f4d3f6fe8644a50f79364cc1da3b1
Daksh Yadav <dakshdav@gmail.com>
2025-12-08 00:31:39 +0530

    Basic routing done and setup done

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const registerUserData = z.object({
+  name: z.string().min(3),
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+export type RegisterUserData = z.infer<typeof registerUserData>;
+
+export const loginUserData = z.object({
+  email: z.email().min(3),
+  password: z.string().min(6),
+});
+
+export type LoginUserData = z.infer<typeof loginUserData>;
