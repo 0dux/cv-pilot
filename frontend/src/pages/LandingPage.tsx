4ac50a4ecd34afe346d5a459cafd10ec5fd3af1a
Daksh Yadav <dakshdav@gmail.com>
2025-12-11 23:25:35 +0530

    Setting up ui libraries

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,8 +1,28 @@
+import { FloatingNav } from "@/components/ui/floating-navbar";
 
 const LandingPage = () => {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div className="min-h-screen bg-black text-white">
+      <FloatingNav navItems={navItems} />
 
-export default LandingPage
+      <div className="min-h-screen">1st div</div>
+      <div className="min-h-screen bg-red-600"></div>
+    </div>
+  );
+};
+
+export default LandingPage;
